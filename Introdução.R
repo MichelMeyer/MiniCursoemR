a5a0fd1e3ddbfb33dc9fa97bdd23e723f3ea109a
MichelMeyer <michel.ricardo.meyer@gmail.com>
2019-09-10 19:25:04 -0300

    Create Introdução.R

---
+++ Introdução.R
@@ -0,0 +1,37 @@
+
+#####################################
+### BEM VINDOS AO MINI CURSO DE R ###
+#####################################
+
+### Porque utilizar o R? ###
+# O R é um software livre, com constantes atualizações e comunidade muito ativa;
+# Os métodos estatísticos mais recentes estão disponíveis em pacotes de R;
+# Uma das principais linguagens usadas em ciências de dados;
+
+
+### Qual a forma mais interessante de aprender o R? ###
+# O método mais interessante e funcional de aprender o R e qualquer linguagem de programação
+# é tendo um problema para resolver utilizando a linguagem;
+
+
+### Qual o problema que queremos resolveremos neste curso ###
+# Encontrar uma estratégia lucrativa para investimento na bolsa de valores;
+# Análizar os resultados desta estratégia;
+
+
+### O que precisamos saber para concluir este objetivos ###
+# Fazer operações aritméticas;
+# Fazer operações lógicas;
+# Criar objetos;
+# Fazer operações com estes objetos;
+# Onde encontrar dados dos preços dos ativos;
+# Como verificar a qualidade dos dados;
+# Criar indicadores em cima destes dados;
+# Criar estratégias com estes indicadores;
+# Verificar o retorno das estratégias;
+
+
+#######################
+### Vamos a Prática ###
+#######################
+
